9230262ef400a146bdb6013a09a8d82bbee6642c
Debanshu Banerjee <79299979+stochasticmaterialism@users.noreply.github.com>
2021-08-12 12:13:37 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,6 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
-
 VERSION = '0.0.11'
 DESCRIPTION = 'Digital Image Correlation Adapted for Damage Mechanics and Cracks'
 LONG_DESCRIPTION = 'Stereo Calibration'
@@ -19,7 +14,7 @@ setup(
     author_email="<debanshu.ju.metallurgy@gmail.com>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     install_requires=['opencv-python'],
     keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
