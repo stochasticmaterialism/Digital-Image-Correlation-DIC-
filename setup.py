1740c6181cc79ed3f7d6a029b2515a22293a8358
Debanshu Banerjee <79299979+stochasticmaterialism@users.noreply.github.com>
2021-08-12 12:09:43 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,34 @@
-1
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.11'
+DESCRIPTION = 'Digital Image Correlation Adapted for Damage Mechanics and Cracks'
+LONG_DESCRIPTION = 'Stereo Calibration'
+
+# Setting up
+setup(
+    name="DIC_LIPEC",
+    version=VERSION,
+    author="Debanshu Banerjee",
+    author_email="<debanshu.ju.metallurgy@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['opencv-python'],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
