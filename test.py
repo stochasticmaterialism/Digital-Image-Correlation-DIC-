a455fbe94262fc55142f65607e1920606f6b2960
Debanshu Banerjee <79299979+stochasticmaterialism@users.noreply.github.com>
2021-08-12 21:43:39 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ def main():
     location1=r"C:\Users\DEBANSHU BANERJEE\Captured Framez"
     location2=r"C:\Users\DEBANSHU BANERJEE\Calibration Templates"
     location3="Calibration Template.jpg"
-    point,special_points,parameters1,parameters2=calibration_1(location1,location2,location3,0.6,0.5)
+    point,special_points,parameters1,parameters2=SingleSurfaceCalibration(location1,location2,location3,0.6,0.5)
     
 if __name__ == "__main__":
     main()
