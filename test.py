0828d1817fb89074ee215801c8624242175114b7
Debanshu Banerjee <79299979+stochasticmaterialism@users.noreply.github.com>
2021-08-14 13:52:25 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 from DIC_LIPEC import *
 
 def main():
-    location1=r"C:\Users\DEBANSHU BANERJEE\Captured Framez"
-    location2=r"C:\Users\DEBANSHU BANERJEE\Calibration Templates"
-    location3="Calibration Template.jpg"
-    point,special_points,parameters1,parameters2=SingleSurfaceCalibration(location1,location2,location3,0.6,0.5)
+    CaptureFrames()
+    location1=r"Enter Location of Captured Frames"
+    location2=r"Enter Location of Templates for Special Points"
+    location3="Name of Template for Normal Point.Format"
+    type1='*.Image Format of Captured Frames'
+    type1='*.Image Format of Templates for Special Points'
+    threshold1=#Define Threshold for DEtecting Normal Points
+    threshold2=#Define Threshold for DEtecting Special Points
+    point,special_points,parameters1,parameters2=SingleSurfaceCalibration(location1,type1,location2,type2,location3,threshold1,threshold2)
     
 if __name__ == "__main__":
     main()
